e67b89bda70f05dfad499ee797c9450d6ecd4aca
Pasha <72137640+pKilba@users.noreply.github.com>
2021-09-02 18:33:26 +0300

    Update Main.java

--- src/main/java/com/epam/hadler/start/Main.java
+++ src/main/java/com/epam/hadler/start/Main.java
@@ -10,14 +10,11 @@ import com.epam.hadler.service.parser.impl.TextParser;
 import com.epam.hadler.service.parser.impl.WordParser;
 import com.epam.hadler.service.reader.impl.ConsoleDataAcquirer;
 import com.epam.hadler.service.reader.impl.FileTextReader;
-
 import org.apache.logging.log4j.Logger;
 
 public class Main {
 
-
     private static final Logger logger = LogManager.getLogger();
-
 
     public static void main(String[] args) throws InCorrectTypeCompositeException, Exception {
 
@@ -25,10 +22,6 @@ public class Main {
         String str = fileTextReader.read(new ConsoleDataAcquirer().getFilePath());
         TextParser pr = new TextParser(new ParagraphParser(new SentenceParser(new WordParser())));
         TextComposite textComposite = pr.parse(str);
-
         Calculator calculator = new Calculator();
-
-
     }
 }
-//test
